7db3ac0212823836c96a80c17a71e4f000b9340e
huangruibang <2798549720@qq.com>
2026-07-27 10:55:18 +0800

    fix(rust): import Write trait for StdoutLock writeln in fs-probe
    The --browser-fs-probe mode uses writeln!(stdout_lock, ...) which
    requires std::io::Write to be in scope.  Compiled locally (macOS has
    the trait transitively) but failed on the Linux CI runner:
    
      error[E0599]: cannot write into std::io::StdoutLock<'_>
    
    Adding Write to the existing use std::io import.

--- rust/khaos-core/src/bin/khaos-sandbox-launcher.rs
+++ rust/khaos-core/src/bin/khaos-sandbox-launcher.rs
@@ -4,7 +4,7 @@
 mod linux {
     use std::env;
     use std::ffi::CString;
-    use std::io;
+    use std::io::{self, Write};
     use std::os::unix::ffi::OsStrExt;
     use std::path::{Path, PathBuf};
 
